6fe77669737c7d4541433586160664bad5b74b55
Emilien Cuny <emilien.cuny0312@gmail.com>
2023-03-03 16:20:38 +0100

    beaucoup de changements

--- crm_python.py
+++ crm_python.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flaskext.mysql import MySQL
 
-app = Flask(__name__)
+app = Flask(__name__)   
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'crm'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost:3307'
+
+mysql = MySQL()
+mysql.init_app(app)
+
+app.secret_key ="a773df62bbef3b540055f1689cde7d04577da32ed8b7b68df8bd5278b3972a3c"
 
 @app.route('/')
 def index():
+    session['user_is_connected'] = False
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO CRM VALUES ('test')")
+    conn.close()
     return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 @app.route('/register')
 def register():
